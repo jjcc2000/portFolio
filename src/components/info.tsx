145d41f48494e1a8d31939c7a9647404ea381209
esJohan <johanchaconchacon@gmail.com>
2025-08-06 13:31:06 -0600

    edit the info component

--- src/components/info.tsx
+++ src/components/info.tsx
@@ -10,18 +10,22 @@ export default function Info() {
           className="h-32 w-32 rounded-full border-4 border-b-black object-cover"
         />
         <h1 className="max-w-xl text-center text-base leading-relaxed sm:text-left sm:text-lg sm:leading-relaxed">
-          Hi, I'm Johan a FullStack-Web3 engineer building at the intersection
-          of <span className="text-indigo-300">smart contracts</span> and
-          scalable systems. I craft frontends with{" "}
-          <span className="text-sky-300">React</span> and{" "}
+          Hi, I’m Johan a Full-Stack--Web3 Engineer building at the
+          intersection of{" "}
+          <span className="text-indigo-300">Smart Contract</span>, robust{" "}
+          <span className="text-lime-400">backends</span>, and high-performance
+          <span className="text-violet-400"> frontends</span>. I create scalable, secure, and user-focused applications from end to end. I use <span className="text-sky-300">React</span> and{" "}
           <span className="text-cyan-300">Tailwind</span>, build backends with
           <span className="text-green-300"> Node.js</span> and{" "}
           <span className="text-blue-300">Golang</span>, and deploy apps using{" "}
           <span className="text-purple-300">Docker</span>,{" "}
-          <span className="text-teal-300">Kubernetes</span>, and{" "} 
+          <span className="text-teal-300">Kubernetes</span>, and{" "}
           <span className="text-orange-300">AWS</span>.
         </h1>
       </div>
     </div>
   );
+}
+{
+  /* of <span className="text-indigo-300">Smart Contracts</span> and */
 }
