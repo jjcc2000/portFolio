cee78186c9a4e00eabef0538f268fb6fabdd1bed
esJohan <johanchaconchacon@gmail.com>
2025-08-16 12:12:13 -0600

    adjust on the front

--- src/components/info.tsx
+++ src/components/info.tsx
@@ -12,7 +12,7 @@ export default function Info() {
         <h1 className="max-w-xl text-center text-base leading-relaxed sm:text-left sm:text-lg sm:leading-relaxed">
           Hi, I’m Johan a Full-Stack--Web3 Engineer building at the intersection
           of <span className="text-indigo-300">Smart Contract</span>, with{" "}
-          <span className="text-fuchsia-300">Solidity</span>robust{" "}
+          <span className="text-fuchsia-300">Solidity</span> robust{" "}
           <span className="text-lime-400">backends</span>, and high-performance
           <span className="text-violet-400"> frontends</span>. I create
           scalable, secure, and user-focused applications from end to end. I use{" "}
@@ -21,7 +21,7 @@ export default function Info() {
           <span className="text-green-300"> Node.js</span> and{" "}
           <span className="text-blue-300">Golang</span>, and deploy apps using{" "}
           <span className="text-purple-300">Docker</span>,{" "}
-          <span className="text-teal-300">Kubernetes</span>, and{" "}
+          <span className="text-teal-300">Kubernetes</span>, on{" "}
           <span className="text-orange-300">AWS</span>.
         </h1>
       </div>
